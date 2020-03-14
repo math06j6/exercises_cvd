80fbbccdde86ec0895fcd87add6b2967a7336ef6
mathias <math06j6@stud.kea.dk>
2020-03-14 17:23:43 +0100

    restructuring local folders

--- getNameParts/get_name_parts.js
+++ getNameParts/get_name_parts.js
@@ -4,7 +4,15 @@ window.addEventListener("DOMContentLoaded", start);
 function start() {
   console.log("start");
 
-  const fullName = "john doe johnjohn johnson";
+  let capitalize = function(fullName) {
+    fullName = fullName.toLowerCase().split(" ");
+    for (let i = 0; i < fullName.length; i++) {
+      fullName[i] = fullName[i].charAt(0).toUpperCase() + fullName[i].slice(1);
+    }
+    return fullName.join(" ");
+  };
+
+  const fullName = capitalize("john doe johnjohn johnson");
   let firstName = fullName.substring(0, fullName.indexOf(" "));
   let lastName = fullName.substring(fullName.lastIndexOf(" ") + 1);
   let middleName = fullName.substring(fullName.indexOf(" ") + 1, fullName.lastIndexOf(" "));
